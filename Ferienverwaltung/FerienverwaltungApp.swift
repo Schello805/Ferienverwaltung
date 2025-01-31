f2dfb2145a5fefeef00afcc2ae079f22a56166f3
Michael <28543330+Schello805@users.noreply.github.com>
2025-01-31 16:10:22 +0100

    Initial Commit

---
+++ Ferienverwaltung/FerienverwaltungApp.swift
@@ -0,0 +1,17 @@
+//
+//  FerienverwaltungApp.swift
+//  Ferienverwaltung
+//
+//  Created by Michael Schellenberger on 31.01.25.
+//
+
+import SwiftUI
+
+@main
+struct FerienverwaltungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FerienverwaltungTests/FerienverwaltungTests.swift
@@ -0,0 +1,17 @@
+//
+//  FerienverwaltungTests.swift
+//  FerienverwaltungTests
+//
+//  Created by Michael Schellenberger on 31.01.25.
+//
+
+import Testing
+@testable import Ferienverwaltung
+
+struct FerienverwaltungTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
